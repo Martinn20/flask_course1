2d110b42138bad0368396f219661fbd754831900
MartinLazarevski <martin.lazarevski96@gmail.com>
2021-12-10 12:51:02 +0100

    adding postgres

--- app_restful.py
+++ app_restful.py
@@ -5,11 +5,10 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-
-
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"]= os.environ.get("DATABASE_URL", "sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "adlk12k3998fJAh1sx00928GjjaghGoLol1ksqpfzZmasijfhqmhdhfMMnVA12Jaz"
 api = Api(app)
