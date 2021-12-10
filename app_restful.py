51212939d6dee35b9b4508e437968a975f5501be
MartinLazarevski <martin.lazarevski96@gmail.com>
2021-12-10 12:35:57 +0100

    small fix

--- app_restful.py
+++ app_restful.py
@@ -5,7 +5,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
+
 
 
 app = Flask(__name__)
@@ -21,10 +21,8 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store, "/store/<string:name>")
 api.add_resource(StoreList, "/stores")
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(debug=True)
